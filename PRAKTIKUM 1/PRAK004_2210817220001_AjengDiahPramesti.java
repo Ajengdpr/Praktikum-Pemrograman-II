72c482bc1f8e01e6a32baa58c7e24060afa876b0
Ajengdpr <114990194+Ajengdpr@users.noreply.github.com>
2023-09-19 00:16:20 +0800

    Add files via upload

---
+++ PRAKTIKUM 1/PRAK004_2210817220001_AjengDiahPramesti.java
@@ -0,0 +1,5 @@
+public class PRAK004_2210817220001_AjengDiahPramesti {
+    public static void main(String[] args) {
+        System.out.println("Perkenalkan, Nama Saya Ajeng Diah Pramesti, Umur Saya 19 Tahun");
+    }
+}
